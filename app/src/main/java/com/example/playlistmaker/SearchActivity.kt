ded7c34d4cc368269b589c5c57dd6bcccf736f00
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-03-24 16:42:02 +0300

    HomeWork

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,92 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 
 class SearchActivity : AppCompatActivity() {
+
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
+    private lateinit var searchEditText: EditText
+
+    @SuppressLint("ClickableViewAccessibility", "UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<Button>(R.id.button_back)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+
+
+        searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val searchIcon = resources.getDrawable(R.drawable.search, null)
+        searchIcon.setBounds(
+            0,
+            0,
+            searchIcon.intrinsicWidth,
+            searchIcon.intrinsicHeight
+        )
+
+
+        if (searchEditText.text.isNullOrEmpty()) {
+            searchEditText.setCompoundDrawables(null, null, null, null)
+        }
+
+        searchEditText.setOnClickListener {
+            searchEditText.requestFocus()
+        }
+
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    searchEditText.setCompoundDrawables(null, null, null, null)
+                } else {
+                    searchEditText.setCompoundDrawables(searchIcon, null, null, null)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Not used
+            }
+        })
+
+        searchEditText.setOnTouchListener { view, event ->
+            val drawable =
+                searchEditText.compoundDrawables[0]
+            if (!searchEditText.text.isNullOrEmpty() && event.x <= searchEditText.paddingLeft + drawable.intrinsicWidth) {
+                searchEditText.setText("")
+                return@setOnTouchListener true
+            }
+            false
+        }
+        if (savedInstanceState != null) {
+            val text = savedInstanceState.getString(SEARCH_TEXT)
+            searchEditText.setText(text)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchEditText.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val text = savedInstanceState.getString(SEARCH_TEXT)
+        searchEditText.setText(text)
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.content.IntentSender.OnFinished
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,39 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<Button>(R.id.button_back)
-
         backButton.setOnClickListener {
             finish()
         }
+
+        val shareButton = findViewById<TextView>(R.id.share_app)
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Курс Android-разработчик от Яндекс Практикума")
+                intent.putExtra(
+                    Intent.EXTRA_TEXT,"https://practicum.yandex.ru/profile/android-developer/")
+                startActivity(Intent.createChooser(intent, "Отправить ссылку через"))
+
+        }
+
+        val writeSupportButton = findViewById<TextView>(R.id.write_support)
+        writeSupportButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("vasil9202@yandex.ru"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            startActivity(shareIntent)
+        }
+
+        val userAgreementButton = findViewById<TextView>(R.id.user_agreement)
+        userAgreementButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
+
 
     }
 }
