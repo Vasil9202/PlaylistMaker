93b88ceb4ce3d2ee490a861c6d8f9141f89cb0bc
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-04-17 08:34:42 +0300

    HomeWork 11 fix

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -38,7 +38,6 @@ class SearchActivity : AppCompatActivity() {
     private lateinit var netErrorText: TextView
     private lateinit var updateButton: ImageButton
     private val iTunes = "https://itunes.apple.com"
-   // private lateinit var tracks: List<Track>
     private val tracks = ArrayList<Track>()
 
 
@@ -84,12 +83,6 @@ class SearchActivity : AppCompatActivity() {
         findNothingImg.visibility = View.GONE
 
         findNothingText.visibility = View.GONE
-
-        netErrorImg.visibility = View.GONE
-
-        netErrorText.visibility = View.GONE
-
-        updateButton.visibility = View.GONE
 
         if (searchEditText.text.isNullOrEmpty()) {
             searchEditText.setCompoundDrawablesWithIntrinsicBounds(searchIcon, null, null, null)
@@ -142,46 +135,9 @@ class SearchActivity : AppCompatActivity() {
         }
 
         recyclerView  = findViewById<RecyclerView>(R.id.trackList)
-    /*    tracks = listOf(
-            Track(
-                "Smells Like Teen Spirit", "Nirvana", "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/" +
-                        "7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean", "Michael Jackson", "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/" +
-                        "38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/" +
-                        "1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/" +
-                        "7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/" +
-                        "a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-
-     */
-        //tracks = emptyList()
         trackAdapter = TrackAdapter(tracks)
         recyclerView.adapter = trackAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-        //recycler.adapter = TrackAdapter(emptyList())
 
         searchEditText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
@@ -217,7 +173,6 @@ class SearchActivity : AppCompatActivity() {
                     tracks.clear()
                     if (response.body()?.results?.isNotEmpty() == true) {
                         tracks.addAll(response.body()?.results!!)
-                        trackAdapter.updateData(tracks)
                         trackAdapter.updateData(tracks)
                     }
                     else{
